1721c327f7db101c54b03e90016242535f8af647
Tomohiko Himura <eiel.hal@gmail.com>
2013-01-05 13:38:24 +0900

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WTMSummary::Application.config.secret_token = '3e49663709bde55d4d6c6c0d3f1793b4f85584c34da730206b64ab9b46c63871d7641e94619c674eaaf49e84e238459ff2f0c1233c55305755a33076ac32c927'
