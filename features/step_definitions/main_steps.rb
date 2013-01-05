2788d12b1d7394f311bae520058225d3f5b19978
Tomohiko Himura <eiel.hal@gmail.com>
2013-01-05 14:09:20 +0900

    step ""にアクセスするを実装

--- features/step_definitions/main_steps.rb
+++ features/step_definitions/main_steps.rb
@@ -4,8 +4,8 @@
   Event.create
 end
 
-もし /^"(.*?)"に訪問する$/ do |arg1|
-  pending # express the regexp above with the code you wish you had
+もし /^"(.*?)"に訪問する$/ do |path|
+  visit(path)
 end
 
 ならば /^最新の勉強会の情報が表示されている$/ do
