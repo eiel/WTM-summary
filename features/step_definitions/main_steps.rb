1f0f605868bac17d492dc75db2973eac2d54af93
Tomohiko Himura <eiel.hal@gmail.com>
2013-01-05 14:33:30 +0900

    stepを もうちょっと具体的にかいた

--- features/step_definitions/main_steps.rb
+++ features/step_definitions/main_steps.rb
@@ -9,5 +9,11 @@ end
 end
 
 ならば /^最新の勉強会の情報が表示されている$/ do
-  expect( page ).to have_content('第53回「WEB TOUCH MEETING」')
+  expect( page ).to have_content('第52回「WEB TOUCH MEETING」')
+  expect( page ).to have_content('Webに活かす映像制作術')
+  expect( page ).to have_content('INDeVisual 浅野')
+  expect( page ).to have_content('あなたのつくったWEBサイト、生きてますか？')
+  expect( page ).to have_content('大林')
+  expect( page ).to have_content('自由過ぎるCMS、concrete5を上手く使うためのお作法')
+  expect( page ).to have_content('コンクリートファイブ ジャパン株式会社 佐々木多生')
 end
